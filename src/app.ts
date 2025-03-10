9bb974c26d753fccb1a3b2790e1b602644ccd924
rrc_w2024_kiran <ksidhu10@academic.rrc.ca>
2025-03-10 14:14:03 -0500

    feat: initialized Express app and configured middleware

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import morgan from 'morgan';
+import  userRoutes  from './api/v1/routes/userRoute';
+import  loanRoutes from './api/v1/routes/loanRoutes';
+
+const app = express();
+const port = 3000;
+
+app.use(morgan('dev'));  
+app.use(express.json()); 
+
+// Routes
+app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/loans', loanRoutes);
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
